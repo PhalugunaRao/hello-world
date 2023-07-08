61dc3d7f305d770d598644356cb82d8bb8d5934b
PhalgunaRao <phalu900@gmail.com>
2023-07-08 22:51:08 +0530

    conditional based render

--- src/components/UserGreetings.js
+++ src/components/UserGreetings.js
@@ -13,17 +13,43 @@ import React, { Component } from 'react'
 
 
   render() {
-    if(this.state.isLoggedIn){
-      return(
-        <div>Welocme Geethu</div>
-      )
-    }else{
-      return (
-          <div>Welcome Guest</div>
-       
-      )
 
-    }
+    return this.state.isLoggedIn && <div>Welcome Geethu</div>
+
+    //Terneray operator
+    // return(
+    //   this.state.isLoggedIn ? (
+    //   <div>Welcome Geethu</div>
+    //   ) : ( 
+    //   <div>Welcome Guest</div>
+    //   )
+     
+    // )
+
+    //Variable conditional
+
+    // let message
+    // if(this.state.isLoggedIn){
+    //   message = <div>Welcome Geethu</div>
+    // }else{
+    //   message = <div>Welcome Guest</div>
+    // }
+    // return <div>{message}</div>
+
+
+
+    //if else
+    // if(this.state.isLoggedIn){
+    //   return(
+    //     <div>Welocme Geethu</div>
+    //   )
+    // }else{
+    //   return (
+    //       <div>Welcome Guest</div>
+       
+    //   )
+
+    // }
   }
 }
 
