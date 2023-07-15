aa851c00c54d2fea708162ae06cd09207ead46e4
PhalgunaRao <phalu900@gmail.com>
2023-07-15 23:28:01 +0530

    Active Links

--- src/routcomponent/Navbar.js
+++ src/routcomponent/Navbar.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+
+  const navLinkStyle= ({isActive}) =>{
+
+    return{
+      fontWeight : isActive? 'bold' :'normal',
+      fontDecoration: isActive?'none':'underline',
+    }
+
+  }
+
+
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
+      <NavLink style={navLinkStyle} to='/'>
+        Home
+      </NavLink>
+      <NavLink style={navLinkStyle} to='/about'>
+        About
+      </NavLink>
     </nav>
   )
 }
