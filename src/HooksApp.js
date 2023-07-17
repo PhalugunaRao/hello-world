7980ebc9baf48fdbc8fa90c332bb770067a9c35c
PhalgunaRao <phalu900@gmail.com>
2023-07-17 22:47:29 +0530

    useMemo

---
+++ src/HooksApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HooksApp = () => {
+  return (
+    <div>
+      <h1>Counter : {}</h1>
+      <button>Increase</button>
+    </div>
+  )
+}
+
+export default HooksApp
