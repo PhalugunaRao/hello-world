b9a9d3dde603066724f2fe42dd1458c683db6bf8
PhalgunaRao <phalu900@gmail.com>
2023-07-18 16:41:10 +0530

    Pseudo classes

--- src/styledComponent/Button/Button.styles.js
+++ src/styledComponent/Button/Button.styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const StyledButton = styled.button`
 border:2px solid #4caf50;
 background-color: ${(props) => props.variant==='outline' ? '#FFF' : '#4caf50'};
-color: ${(props) => props.variant==='outline' ? '#4caf50' : '#FFF'};;
+color: ${(props) => props.variant==='outline' ? '#4caf50' : '#FFF'};
 padding: 15px 32px;
 text-align: center;
 text-decoration:none;
@@ -11,6 +11,10 @@ display: inline-block;
 font-size: 16px;
 cursor: pointer;
 transition:0.5s all ease-out;
+&:hover{
+background-color: ${(props) => props.variant !=='outline' ? '#FFF' : '#4caf50'};
+color: ${(props) => props.variant !=='outline' ? '#4caf50' : '#FFF'};
+}
 `
 
 export const FancyButton = styled(StyledButton)`
