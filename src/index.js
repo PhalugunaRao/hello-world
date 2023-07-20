65e1e499365fc3860a4593fbe78ad6c19e7a4ac4
PhalgunaRao <phalu900@gmail.com>
2023-07-20 15:10:46 +0530

    use state

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import App from './App';
 import RouteApp from './RoutApp'
 import StyleApp from './StyleApp'
 import reportWebVitals from './reportWebVitals';
+import HooksApp from './Hooks/HooksApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <StyleApp></StyleApp>
+    <HooksApp/>
+    {/* <StyleApp></StyleApp> */}
     {/* <BrowserRouter>
      <RouteApp />
     </BrowserRouter> */}
