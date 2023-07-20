aa3374439444f564e46e4a070a448b2e939ffc50
PhalgunaRao <phalu900@gmail.com>
2023-07-20 15:56:06 +0530

    useeffects

--- src/Hooks/HooksApp.js
+++ src/Hooks/HooksApp.js
@@ -2,47 +2,64 @@ import React, { useEffect, useState } from 'react'
 import { json } from 'react-router-dom'
 
 function HooksApp() {
-  const [resourceType,setResourceType] = useState('posts')
-  const [items,setItems] = useState([])
+  const[windowWidth,setwindowWidth] = useState(window.innerWidth)
+
+  const handleResize = () =>{
+    setwindowWidth(window.innerWidth)
+  }
+
   useEffect(() =>{
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-    .then(response => response.json())
-    .then(json => setItems(json))
-  },[resourceType])
-  
-  const [count,setCount] = useState(() =>{
-    console.log('run function')
-    return 4
-  })
-  const [theme,setTheme] = useState('blue')
+    window.addEventListener('resize',handleResize)
 
-  function decrementCount(){
-    setCount(prevCount=> prevCount-1)
-  }
-  function incrementCount(){
-    setCount(prevCount=>prevCount+1)
+  },[])
 
-  }
-  return (
-    <>
+  return(
     <div>
-      <button onClick={()=>setResourceType('posts')}>Posts</button>
-      <button onClick={() => setResourceType('users')}>Users</button>
-      <button onClick={()=>setResourceType('comments')}>Comments</button>
+      {windowWidth}
+
     </div>
-    <h1>{resourceType}</h1>
-    {
-      items.map(items =>{
-        return <pre>{JSON.stringify(items)}</pre>
-      })
-    }
-    {/* <button onClick={decrementCount}>-</button>
-    <span>{count}</span>
-    <span>{theme}</span>
-    <button onClick={incrementCount}>+</button> */}
-    </>
-    
   )
+  // const [resourceType,setResourceType] = useState('posts')
+  // const [items,setItems] = useState([])
+  // useEffect(() =>{
+  //   fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+  //   .then(response => response.json())
+  //   .then(json => setItems(json))
+  // },[resourceType])
+  
+  // const [count,setCount] = useState(() =>{
+  //   console.log('run function')
+  //   return 4
+  // })
+  // const [theme,setTheme] = useState('blue')
+
+  // function decrementCount(){
+  //   setCount(prevCount=> prevCount-1)
+  // }
+  // function incrementCount(){
+  //   setCount(prevCount=>prevCount+1)
+
+  // }
+  // return (
+  //   <>
+  //   <div>
+  //     <button onClick={()=>setResourceType('posts')}>Posts</button>
+  //     <button onClick={() => setResourceType('users')}>Users</button>
+  //     <button onClick={()=>setResourceType('comments')}>Comments</button>
+  //   </div>
+  //   <h1>{resourceType}</h1>
+  //   {
+  //     items.map(items =>{
+  //       return <pre>{JSON.stringify(items)}</pre>
+  //     })
+  //   }
+  //   {/* <button onClick={decrementCount}>-</button>
+  //   <span>{count}</span>
+  //   <span>{theme}</span>
+  //   <button onClick={incrementCount}>+</button> */}
+  //   </>
+    
+  // )
 }
 
 export default HooksApp
